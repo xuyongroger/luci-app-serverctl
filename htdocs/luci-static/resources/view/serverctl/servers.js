3e3ed379a2b31103ad4b73593acb03fdbefda48c
xuyongroger <35016948+xuyongroger@users.noreply.github.com>
2026-09-19 17:10:03 +0800

    777

--- htdocs/luci-static/resources/view/serverctl/servers.js
+++ htdocs/luci-static/resources/view/serverctl/servers.js
@@ -6,30 +6,29 @@ return view.extend({
 	render: function() {
 		var m, s, o;
 
-		// 将页面标题修改为“服务器信息”
-		m = new form.Map('serverctl', _('服务器信息'), _('在此处维护和管理您的局域网服务器清单。保存后即可在控制面板的下拉列表和定时任务弹窗中选用。'));
+		m = new form.Map('serverctl', _('Server Information'), _('Maintain and manage your LAN server list here. After saving, it can be selected in the control panel dropdown and scheduled task modals.'));
 
-		s = m.section(form.GridSection, 'server', _('服务器列表'));
+		s = m.section(form.GridSection, 'server', _('Server List'));
 		s.addremove = true;
 		s.anonymous = true;
 		s.rowcolors = true;
 
-		o = s.option(form.Value, 'name', _('服务器别名'));
+		o = s.option(form.Value, 'name', _('Server Alias'));
 		o.rmempty = false;
 
-		o = s.option(form.Value, 'ip', _('IP 地址'));
+		o = s.option(form.Value, 'ip', _('IP Address'));
 		o.datatype = 'ip4addr';
 		o.rmempty = false;
 
-		o = s.option(form.Value, 'mac', _('MAC 地址'));
+		o = s.option(form.Value, 'mac', _('MAC Address'));
 		o.datatype = 'macaddr';
 		o.rmempty = false;
 
-		o = s.option(form.Value, 'user', _('SSH 用户名'));
+		o = s.option(form.Value, 'user', _('SSH Username'));
 		o.default = 'root';
 		o.rmempty = false;
 
-		o = s.option(form.Value, 'password', _('SSH 密码'));
+		o = s.option(form.Value, 'password', _('SSH Password'));
 		o.password = true;
 		o.rmempty = false;
 
